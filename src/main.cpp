5b1ab12f5a62a0b7f480c3a73247dcc85fd9513d
laurensiusyuda <yudalaurensius87@gmail.com>
2023-06-05 21:18:02 +0700

    update fuzzy logic

--- src/main.cpp
+++ src/main.cpp
@@ -34,8 +34,10 @@ DallasTemperature sensors(&oneWire);
 
 int sensorValue = 0;
 unsigned long int avgValue;
-float b;
+
 int buf[10], temp;
+float minr[27];
+float Rule[27];
 
 float suhu_rendah;
 float suhu_sedang;
@@ -217,7 +219,7 @@ unsigned char suhuSejuk()
   return suhu_sedang;
 }
 
-unsigned char suhuDingin()
+unsigned char suhuPanas()
 {
   if (baca_suhu() <= 26)
   {
@@ -270,6 +272,7 @@ unsigned char salinitasPayau()
   {
     salinitas_payau = 0;
   }
+  return salinitas_payau;
 }
 
 unsigned char salinitasAsin()
@@ -286,6 +289,7 @@ unsigned char salinitasAsin()
   {
     salinitas_asin = 1;
   }
+  return salinitas_asin;
 }
 
 // fuzzyfikasi sensor pH
@@ -309,20 +313,21 @@ unsigned char phNormal()
 {
   if (baca_pH() <= 6.5)
   {
-    salinitas_payau = 0;
+    ph_netral = 0;
   }
   else if (baca_pH() >= 6.5 && baca_pH() <= 7)
   {
-    salinitas_payau = (baca_pH() - 6.5) / (7 - 6.5);
+    ph_netral = (baca_pH() - 6.5) / (7 - 6.5);
   }
   else if (baca_pH() >= 7 && baca_pH() <= 8)
   {
-    salinitas_payau = 1;
+    ph_netral = 1;
   }
   else if (baca_pH() >= 9)
   {
-    salinitas_payau = 0;
+    ph_netral = 0;
   }
+  return ph_netral;
 }
 unsigned char phBasa()
 {
@@ -340,6 +345,19 @@ unsigned char phBasa()
   }
   return ph_asam;
 }
+// membuat fungsi void fuzzyfikasi
+void fuzzyfikasi()
+{
+  suhuRendah();
+  suhuSejuk();
+  suhuPanas();
+  salinitasPayau();
+  salinitasTawar();
+  salinitasAsin();
+  phAsam();
+  phNormal();
+  phBasa();
+}
 
 // membuat fungsi min
 float Min(float a, float b, float c)
@@ -356,6 +374,66 @@ float Min(float a, float b, float c)
   {
     return c;
   }
+}
+
+// komposisi aturan
+void rule()
+{
+  fuzzyfikasi();
+  minr[0] = Min(suhu_rendah, salinitas_tawar, ph_basa);    //! buruk
+  minr[1] = Min(suhu_rendah, salinitas_tawar, ph_netral);  //! buruk
+  minr[2] = Min(suhu_rendah, salinitas_tawar, ph_asam);    //! buruk
+  minr[3] = Min(suhu_rendah, salinitas_payau, ph_basa);    //! buruk
+  minr[4] = Min(suhu_rendah, salinitas_payau, ph_netral);  //? cukup
+  minr[5] = Min(suhu_rendah, salinitas_payau, ph_asam);    //! buruk
+  minr[6] = Min(suhu_rendah, salinitas_asin, ph_basa);     //! buruk
+  minr[7] = Min(suhu_rendah, salinitas_asin, ph_netral);   //! buruk
+  minr[8] = Min(suhu_rendah, salinitas_asin, ph_asam);     //! buruk
+  minr[9] = Min(suhu_sedang, salinitas_tawar, ph_basa);    //! buruk
+  minr[10] = Min(suhu_sedang, salinitas_tawar, ph_netral); //? cukup
+  minr[11] = Min(suhu_sedang, salinitas_tawar, ph_asam);   //! buruk
+  minr[12] = Min(suhu_sedang, salinitas_payau, ph_basa);   //? cukup
+  minr[13] = Min(suhu_sedang, salinitas_payau, ph_netral); //* bagus
+  minr[14] = Min(suhu_sedang, salinitas_payau, ph_asam);   //? cukup
+  minr[15] = Min(suhu_sedang, salinitas_asin, ph_basa);    //! buruk
+  minr[16] = Min(suhu_sedang, salinitas_asin, ph_netral);  //! buruk
+  minr[17] = Min(suhu_sedang, salinitas_asin, ph_asam);    //! buruk
+  minr[18] = Min(suhu_tinggi, salinitas_tawar, ph_basa);   //! buruk
+  minr[19] = Min(suhu_tinggi, salinitas_tawar, ph_netral); //! buruk
+  minr[20] = Min(suhu_tinggi, salinitas_tawar, ph_asam);   //! buruk
+  minr[21] = Min(suhu_tinggi, salinitas_payau, ph_basa);   //! buruk
+  minr[22] = Min(suhu_tinggi, salinitas_payau, ph_netral); //? cukup
+  minr[23] = Min(suhu_tinggi, salinitas_payau, ph_asam);   //! buruk
+  minr[24] = Min(suhu_tinggi, salinitas_asin, ph_basa);    //! buruk
+  minr[25] = Min(suhu_tinggi, salinitas_asin, ph_netral);  //! buruk
+  minr[26] = Min(suhu_tinggi, salinitas_asin, ph_asam);    //! buruk
+  Rule[0] = minr[0];
+  Rule[1] = minr[1];
+  Rule[2] = minr[2];
+  Rule[3] = minr[3];
+  Rule[4] = minr[4];
+  Rule[5] = minr[5];
+  Rule[6] = minr[6];
+  Rule[7] = minr[7];
+  Rule[8] = minr[8];
+  Rule[9] = minr[9];
+  Rule[10] = minr[10];
+  Rule[11] = minr[11];
+  Rule[12] = minr[12];
+  Rule[13] = minr[13];
+  Rule[14] = minr[14];
+  Rule[15] = minr[15];
+  Rule[16] = minr[16];
+  Rule[17] = minr[17];
+  Rule[18] = minr[18];
+  Rule[19] = minr[19];
+  Rule[20] = minr[20];
+  Rule[21] = minr[21];
+  Rule[22] = minr[22];
+  Rule[23] = minr[23];
+  Rule[24] = minr[24];
+  Rule[25] = minr[25];
+  Rule[26] = minr[26];
 }
 
 void loop()
